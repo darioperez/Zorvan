6dcbe81d6b1e89dfdc9e6d44b6acd25c5b88a3dc
Dario Perez <darioperez1993@gmail.com>
2022-11-29 23:49:13 -0400

    Add basic express/socketio/typescript messaging server with multiroom capabilities

---
+++ packages/server/src/index.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import { addUser, getUser, deleteUser, getUsers } from "./users";
+
+const app = express();
+const http = createServer(app);
+const io = new Server(http);
+const PORT = process.env.PORT || 5000
+
+
+app.use(cors())
+
+io.on('connection', (socket) => {
+  console.log("User connected");
+
+  socket.on('login', ({ name, room }, callback) => {
+    const { user, error } = addUser(socket.id, name, room)
+    if (error) return callback(error)
+
+    if (user) {
+      socket.join(user.room)
+      socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` })
+    }
+    io.in(room).emit('users', getUsers(room))
+    callback()
+  })
+
+  socket.on('sendMessage', message => {
+    const user = getUser(socket.id)
+    if (user)
+      io.in(user.room).emit('message', { user: user.name, text: message });
+  })
+
+  const logout = () => {
+    console.log("User disconnected");
+    const user = deleteUser(socket.id)
+    if (user) {
+      io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` })
+      io.in(user.room).emit('users', getUsers(user.room))
+    }
+  };
+
+  socket.on("disconnect", logout);
+
+  socket.on("logout", logout);
+})
+
+app.get('/', (req, res) => {
+  res.send("Server is up and running")
+})
+
+http.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+})
